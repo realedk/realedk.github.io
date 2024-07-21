3d92260644cd1c3edab568c157abd0c4b557b666
EDK <edktech@protonmail.com>
2024-07-20 19:05:41 -0500

    updated website

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('discord').addEventListener('click', function() {
+    document.getElementById('discordPopup').style.display = 'block';
+});
+
+function closePopup() {
+    document.getElementById('discordPopup').style.display = 'none';
+}
