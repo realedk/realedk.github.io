c3fd9a5ab1e878fedc6cb43d2013a72115fb2e07
EDK <83384667+ephraimkreighbaum@users.noreply.github.com>
2024-09-28 12:03:23 -0500

    new tohe role guide

---
+++ tohe-roles/script.js
@@ -0,0 +1,8 @@
+function toggleInfo(id) {
+    var info = document.getElementById(id);
+    if (info.style.display === "none" || info.style.display === "") {
+        info.style.display = "block";
+    } else {
+        info.style.display = "none";
+    }
+}
